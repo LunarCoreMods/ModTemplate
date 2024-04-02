dfe3d43616cb21af94480ae45c2634de8d30f3c4
yuko1101 <68993883+yuko1101@users.noreply.github.com>
2024-04-02 20:04:08 +0900

    added LunarCoreRunner plugin

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -1,16 +1,19 @@
 package com.example.examplemod;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.loader.impl.util.log.Log;
+import net.fabricmc.loader.impl.util.log.LogCategory;
 
 import java.util.logging.Logger;
 
 public class ExampleMod implements ModInitializer {
 
     public static final String MOD_NAME = "ExampleMod";
-    public static final Logger LOGGER = Logger.getLogger(MOD_NAME);
+
+    public static final LogCategory LOG_CATEGORY = LogCategory.create(MOD_NAME);
 
     @Override
     public void onInitialize() {
-        LOGGER.info("Initializing " + MOD_NAME);
+        Log.info(LOG_CATEGORY, "Initializing " + MOD_NAME);
     }
 }
